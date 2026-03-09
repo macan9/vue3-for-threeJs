2a45f5128670e26656b3b749ab6fb6abc67b8f76
macan9 <729672251@qq.com>
2026-03-09 10:17:46 +0800

    config

--- public/config/globals_config.js
+++ public/config/globals_config.js
@@ -1,6 +1,8 @@
 export const globals_config ={
     // host_service: "http://localhost:8010", // mock
-    host_service: "http://139.196.104.120:443/" , // 测试地址
+    // host_service: "http://139.196.104.120:443/" , // 测试地址
+    host_service: "http://139.196.158.225:3000/" , // 测试地址
+
     
     gitee_user_config: {
         owner : 'mc150324',
